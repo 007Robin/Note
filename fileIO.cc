7da333938deaca375c88356969cb5295b2317510
007Robin <30958079+007Robin@users.noreply.github.com>
2017-12-28 00:44:08 -0500

    Update fileIO.cc

--- fileIO.cc
+++ fileIO.cc
@@ -16,3 +16,17 @@
               vec.push_back(buf);
       }
   }
+
+void write(string fileName){
+     ofstream out(fileName);
+			  if(out) {
+				    auto n = fileName.find('.');
+				    string s = fileName.substr(0, n);
+				    out << "solid " << s << endl;
+				    for( int i = 0; i < shapes.size(); ++i) {
+					          shapes[i]->write(out);
+				    }
+				    out << "endsolid " << s;
+				    out.close();
+     }
+}
