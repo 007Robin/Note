0e02d3affd3d1a5f8a4883fc72640edf58c0b29a
007Robin <30958079+007Robin@users.noreply.github.com>
2018-02-02 11:21:43 -0500

    Update stack_unwinding_example.cpp
    exception

--- stack_unwinding_example.cpp
+++ stack_unwinding_example.cpp
@@ -78,17 +78,23 @@ End first
 End main
 *／
 ／*
-Let’s examine what happens in this case. The printing of all the start statements is straightforward and doesn’t warrant further explanation. Function last() prints “last throwing int exception” and then throws an int exception. This is where things start to get interesting.
+Let’s examine what happens in this case. The printing of all the start statements is straightforward and doesn’t warrant further explanation. 
+Function last() prints “last throwing int exception” and then throws an int exception. This is where things start to get interesting.
 
-Because last() doesn’t handle the exception itself, the stack begins to unwind. Function last() terminates immediately and control returns to the caller, which is third().
+Because last() doesn’t handle the exception itself, the stack begins to unwind. 
+Function last() terminates immediately and control returns to the caller, which is third().
 
 Function third() doesn’t handle any exceptions, so it terminates immediately and control returns to second().
 
-Function second() has a try block, and the call to third() is within it, so the program attempts to match the exception with an appropriate catch block. However, there are no handlers for exceptions of type int here, so second() terminates immediately and control returns to first().
+Function second() has a try block, and the call to third() is within it, so the program attempts to match the exception with an appropriate catch block. 
+However, there are no handlers for exceptions of type int here, so second() terminates immediately and control returns to first().
 
-Function first() also has a try block, and the call to second() is within it, so the program looks to see if there is a catch handler for int exceptions. There is! Consequently, first() handles the exception, and prints “first caught int exception”.
+Function first() also has a try block, and the call to second() is within it, so the program looks to see if there is a catch handler for int exceptions. 
+There is! Consequently, first() handles the exception, and prints “first caught int exception”.
 
-Because the exception has now been handled, control continues normally at the end of the catch block within first(). This means first() prints “End first” and then terminates normally.
+Because the exception has now been handled, control continues normally at the end of the catch block within first(). 
+This means first() prints “End first” and then terminates normally.
 
-Control returns to main(). Although main() has an exception handler for int, our exception has already been handled by first(), so the catch block within main() does not get executed. main() simply prints “End main” and then terminates normally.
+Control returns to main(). Although main() has an exception handler for int, our exception has already been handled by first(), 
+so the catch block within main() does not get executed. main() simply prints “End main” and then terminates normally.
 *／
